fc3d93617733a7eda5ca031e8b3a08ea35645b5a
vaishnavnegi <lucky@LAPTOP-LSADHVJH.localdomain>
2020-05-14 11:27:09 +0530

    Made changes to math_functions and compute for implementation of factorial function.

--- app/src/models/compute.py
+++ app/src/models/compute.py
@@ -21,6 +21,8 @@ class Compute:
     def ackermann(m, n):
         return
 
-
+    @staticmethod
     def factorial(n):
-        return
+        if n==0 or n==1:
+            return 1
+        return n * factorial(n-1)
--- app/src/views/math_functions.py
+++ app/src/views/math_functions.py
@@ -31,6 +31,9 @@ def fib_n():
 def ack_mn():
     return ""
 
-
+@math.route("/fact", methods=["POST"])
 def fact_n():
-    return ""
+    n=request.form["fact_n"]
+    if not validations.validate_int(n):
+        return "Please enter a valid input!", 400
+    return str(Compute.factorial(int(n)))
